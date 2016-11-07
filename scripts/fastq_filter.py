680f7d98b6174e5d20b4bdb6c8a87f852a20639b
Daniel Bunting <daniel.bunting@earlham.ac.uk>
2016-11-07 22:46:44 +0000

    Remove Bio.SeqIO usage for speed

--- scripts/fastq_filter.py
+++ scripts/fastq_filter.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-import Bio.SeqIO
 import gzip
 import argparse
-import numpy as np
 import io
-from mmap import mmap, ACCESS_READ, ACCESS_WRITE
+
 
 class no_Ns():
     '''Filter any read that contains an N'''
@@ -13,7 +11,7 @@ class no_Ns():
         self.failures = 0
         
     def __call__(self, r1, r2):
-        predicate = not (('N' in r1.seq) or ('N' in r2.seq))
+        predicate = not (('N' in r1) or ('N' in r2))
         if not predicate:
             self.failures += 1
         return predicate
@@ -26,10 +24,11 @@ class exact_length():
     :param L: Read filter length
     '''
     def __init__(self, L=101):
-        self.L = L
+        self.L = L + 1 #Add 1 to count the newline
         self.failures = 0
+        
     def __call__(self, r1, r2):
-        predicate = (len(r1.seq) == self.L) and (len(r2.seq) == self.L)         
+        predicate = (len(r1) == self.L) and (len(r2) == self.L)         
         if not predicate:
             self.failures += 1
         return predicate
@@ -46,44 +45,48 @@ class FastqFilter():
         self.out_r2 = out_r2
         self.filters = filters
         
-        self.apply_mmap()
+        self.apply()
 
-    def apply_streaming(self):
-        with gzip.open(self.in_r1, 'rt') as r1_in_fh, gzip.open(self.in_r2, 'rt') as r2_in_fh:
-            with gzip.open(self.out_r1, 'wt') as r1_out_fh, gzip.open(self.out_r2, 'wt') as r2_out_fh:
-                in_count, out_count = 0,0
-                for r1, r2 in zip(Bio.SeqIO.parse(r1_in_fh, 'fastq'), Bio.SeqIO.parse(r2_in_fh, 'fastq')): 
-                    in_count += 1
-                    if all([f(r1, r2) for f in self.filters]):
-                        out_count += 1
-                        Bio.SeqIO.write(r1, r1_out_fh, 'fastq')
-                        Bio.SeqIO.write(r2, r2_out_fh, 'fastq')
-
-        print("In : {0} Failed: ".format(in_count) + "\t".join([str(f) for f in self.filters]))
-
-    def apply_mmap(self):
-        ## Fully mmap the compressed input files
-        with open(self.in_r1, 'rb', buffering=2**28) as r1_in_gz, open(self.in_r2, 'rb', buffering=2**28) as r2_in_gz:
+    @profile
+    def apply(self):
+        
+        ## Use mid sized caches 134mb to reduce network load
+        with open(self.in_r1, 'rb', buffering=2**27) as r1_in_gz, open(self.in_r2, 'rb', buffering=2**27) as r2_in_gz:
             r1_in_fh = io.TextIOWrapper(gzip.GzipFile(mode='r', fileobj=r1_in_gz))
             r2_in_fh = io.TextIOWrapper(gzip.GzipFile(mode='r', fileobj=r2_in_gz))
             
-            ## Fully buffer the output files.
+            ## Fully buffer the output files, so script is (more) atomic
             r1_buf, r2_buf = io.BytesIO(), io.BytesIO()
-            r1_out_gz = io.TextIOWrapper(gzip.GzipFile(mode='w', fileobj=r1_buf))
-            r2_out_gz = io.TextIOWrapper(gzip.GzipFile(mode='w', fileobj=r2_buf))
+            r1_out_gz, r2_out_gz = gzip.GzipFile(mode='w', fileobj=r1_buf), gzip.GzipFile(mode='w', fileobj=r2_buf)
+            r1_out_text, r2_out_text = io.TextIOWrapper(r1_out_gz),io.TextIOWrapper(r2_out_gz)
             
             in_count, out_count = 0,0
-            for r1, r2 in zip(Bio.SeqIO.parse(r1_in_fh, 'fastq'), Bio.SeqIO.parse(r2_in_fh, 'fastq')): 
+            zipped = zip(r1_in_fh, r2_in_fh)
+            for r1_header, r2_header in zipped:
+                 
+                if r1_header[0] != '@' or r2_header[0] != '@':
+                    print("Not at the start of a record!!")
+                    continue
+                
+                r1_seq, r2_seq = next(zipped)
+                r1_desc, r2_desc = next(zipped)
+                r1_qual, r2_qual = next(zipped)
+                
                 in_count += 1
-                if all([f(r1, r2) for f in self.filters]):
+                if all([f(r1_seq, r2_seq) for f in self.filters]):
                     out_count += 1
-                    Bio.SeqIO.write(r1, r1_out_gz, 'fastq')
-                    Bio.SeqIO.write(r2, r2_out_gz, 'fastq')
+                    r1_out_text.writelines([r1_header, r1_seq, r1_desc, r1_qual])
+                    r2_out_text.writelines([r1_header, r1_seq, r1_desc, r1_qual])
+                
+                if in_count > 1000:
+                    break
                     
-                if in_count % 10000 == 0 :
+                if in_count % 100000 == 0 :
                     print("Done {0}".format(in_count))
                     
             with open(self.out_r1, 'wb') as r1_out_fh, open(self.out_r2, 'wb',) as r2_out_fh:
+                r1_out_gz.close()
+                r2_out_gz.close()
                 r1_out_fh.write(r1_buf.getvalue())
                 r2_out_fh.write(r2_buf.getvalue())
                 
@@ -91,15 +94,7 @@ class FastqFilter():
 
     
 if __name__ == '__main__':
-    #in_r1 = '/tgac/scratch/buntingd/LIB10868/raw_R1.fastq.gz'         
-    #in_r2 = '/tgac/scratch/buntingd/LIB10868/raw_R2.fastq.gz' 
-    #out_r1 = '/tgac/scratch/buntingd/LIB10868/pyfilter_R1.fastq.gz' 
-    #out_r2 = '/tgac/scratch/buntingd/LIB10868/pyfilter_R2.fastq.gz'
-    #
-    #ff = FastqFilter(in_r1, in_r2, out_r1, out_r2, [no_Ns(), exact_length(101)])
-    #ff.apply_mmap()
-    
-    
+        
     parser = argparse.ArgumentParser()
     parser.add_argument('in_R1')
     parser.add_argument('in_R2')

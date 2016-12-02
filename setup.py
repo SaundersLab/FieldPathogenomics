3c092ed839238799bb2029a0b96be530ffe1069c
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2016-12-02 16:00:02 +0000

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "FieldPathogenomics",
+    version = "0.0.0",
+    author = "Daniel Bunintg",
+    author_email = "daniel.bunting@earlham.ac.uk",
+    url = "https://github.com/dnlbunting/FieldPathogenomics/",
+    packages=['src'],
+
+)

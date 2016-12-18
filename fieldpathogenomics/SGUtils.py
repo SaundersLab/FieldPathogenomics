d54975d04ef01cea26169ea95da1be9f922c1a92
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2016-12-18 15:15:06 +0000

    autoPEP8

--- fieldpathogenomics/SGUtils.py
+++ fieldpathogenomics/SGUtils.py
@@ -11,14 +11,15 @@ from fieldpathogenomics.luigi.slurm import SlurmExecutableTask
 from luigi.util import requires, inherits
 from fieldpathogenomics.utils import CheckTargetNonEmpty
 
-picard="java -XX:+UseSerialGC -Xmx{mem}M -jar /tgac/software/testing/picardtools/2.1.1/x86_64/bin/picard.jar"
-python="source /usr/users/ga004/buntingd/FP_dev/dev/bin/activate"
+picard = "java -XX:+UseSerialGC -Xmx{mem}M -jar /tgac/software/testing/picardtools/2.1.1/x86_64/bin/picard.jar"
+python = "source /usr/users/ga004/buntingd/FP_dev/dev/bin/activate"
 
 # Ugly hack
 script_dir = os.path.join(os.path.split(__file__)[0], 'scripts')
 
+
 class ScatterVCF(SlurmExecutableTask):
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Set the SLURM request params for this task
@@ -27,16 +28,16 @@ class ScatterVCF(SlurmExecutableTask):
         self.partition = "tgac-medium"
 
     def run(self):
-        # Avoid Scatter-Work-Gather-Scatter antipattern by doing a last minute check to see if 
+        # Avoid Scatter-Work-Gather-Scatter antipattern by doing a last minute check to see if
         if not self.complete():
             super().run()
         else:
             logger.info("Re-using existing scatter")
-            
+
     def work_script(self):
-        dir,filename = os.path.split(self.output()[0].path)
+        dir, filename = os.path.split(self.output()[0].path)
         base, idx = filename.rsplit('_', maxsplit=1)
-        
+
         return '''#!/bin/bash -e
                 source vcftools-0.1.13;
                 set -euo pipefail
@@ -60,43 +61,45 @@ class ScatterVCF(SlurmExecutableTask):
                            input=self.input().path,
                            N_scatter=len(self.output()))
 
+
 class ScatterBED(luigi.Task, CheckTargetNonEmpty):
 
     def __init__(self, *args, **kwargs):
-        super().__init__( *args, **kwargs)
-    
+        super().__init__(*args, **kwargs)
+
     def run(self):
         with self.input().open() as fin:
-            inp = [(l, int(l.split()[2]) - int(l.split()[1]) ) for l in fin]
-            
+            inp = [(l, int(l.split()[2]) - int(l.split()[1])) for l in fin]
+
         total_seq_len = sum([x[1] for x in inp])
-        perfile = math.ceil(total_seq_len/len(self.output()))        
-        
+        perfile = math.ceil(total_seq_len / len(self.output()))
+
         inp_iter = iter(inp)
         files = 0
-        for i,out in enumerate(self.output()):
+        for i, out in enumerate(self.output()):
             count = 0
             with out.open('w') as fout:
-                for l,c in inp_iter:
+                for l, c in inp_iter:
                     fout.write(l)
                     count += c
                     if count > perfile:
                         break
-                        
-                files+=1                
+
+                files += 1
                 if files == len(self.output()):
-                    #If we're on the final file dump the rest
+                    # If we're on the final file dump the rest
                     fout.writelines([x[0] for x in inp_iter])
+
 
 class GatherVCF(SlurmExecutableTask, CheckTargetNonEmpty):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs) 
+        super().__init__(*args, **kwargs)
         # Set the SLURM request params for this task
         self.mem = 8000
         self.n_cpu = 1
         self.partition = "tgac-medium"
-        
+
     def work_script(self):
         return '''#!/bin/bash
                 picard='{picard}'
@@ -108,34 +111,37 @@ class GatherVCF(SlurmExecutableTask, CheckTargetNonEmpty):
                 
                 mv {output}.temp.vcf.gz {output}
                 
-                '''.format(picard=picard.format(mem=self.mem*self.n_cpu),
+                '''.format(picard=picard.format(mem=self.mem * self.n_cpu),
                            output=self.output().path,
-                           in_flags=  "\\\n".join([" I= "+ x.path for x in self.input()])
+                           in_flags="\\\n".join([" I= " + x.path for x in self.input()])
                            )
+
 
 class GatherCat(SlurmExecutableTask, CheckTargetNonEmpty):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs) 
+        super().__init__(*args, **kwargs)
         # Set the SLURM request params for this task
         self.mem = 4000
         self.n_cpu = 1
         self.partition = "tgac-short"
-        
+
     def work_script(self):
         return '''#!/bin/bash
                 cat {inputs} > {output}.temp
                 mv {output} {output}
                 
                 '''.format(output=self.output().path,
-                           inputs= " \\\n".join([x.path for x in self.input()])
+                           inputs=" \\\n".join([x.path for x in self.input()])
                            )
+
 
 class GatherTSV(luigi.Task, CheckTargetNonEmpty):
     '''Gather TSV files making sure to only record the header line once'''
+
     def run(self):
         with self.output().open('w') as fout:
-            for i,inp in enumerate(self.input()):
+            for i, inp in enumerate(self.input()):
                 with inp.open('r') as fin:
                     lines = fin.readlines()
                     if i == 0:
--- fieldpathogenomics/utils.py
+++ fieldpathogenomics/utils.py
@@ -11,32 +11,38 @@ import inspect
 #                         Testing file emptiness                              #
 ###############################################################################
 
+
 def isNeGz(fname):
     with gzip.open(fname, 'rb') as f:
         data = f.read(1)
     return len(data) > 0
 
+
 def isNePlain(fname):
-    return  os.path.getsize(fname) > 0
-    
+    return os.path.getsize(fname) > 0
+
+
 def isNeBam(fname):
-    r = subprocess.run("set -o pipefail; source samtools-0.1.19 && samtools view {0} | head -c1 ".format(fname), shell=True, stdout=subprocess.PIPE)
+    r = subprocess.run("set -o pipefail; source samtools-0.1.19 && samtools view {0} | head -c1 ".format(
+        fname), shell=True, stdout=subprocess.PIPE)
 
     if r.returncode == 1:
         # samtools will return 1 if fname is not a valid bam
         # Check specifically for 1 rather than !=0 as if
         # The file is bam and non-empty head causes the retcode to be SIGPIPE 141
         raise subprocess.CalledProcessError(r.returncode, str(r.args))
-        
+
     return len(r.stdout) > 0
-    
-extensions_dispath = defaultdict(lambda : isNePlain, {'bam':isNeBam,
-                                                      'gz':isNeGz})
+
+extensions_dispath = defaultdict(lambda: isNePlain, {'bam': isNeBam,
+                                                     'gz': isNeGz})
+
+
 def is_not_empty(fname):
 
     try:
         # Try dispatching to handler determmined by file extension
-        _,ext = fname.rsplit('.', 1)
+        _, ext = fname.rsplit('.', 1)
         return extensions_dispath[ext](fname)
 
     except ValueError:
@@ -46,11 +52,13 @@ def is_not_empty(fname):
             try:
                 return isNeGz(fname)
             except OSError:
-                return  os.path.getsize(fname) > 0
-    
-class CheckTargetNonEmpty(object): 
+                return os.path.getsize(fname) > 0
+
+
+class CheckTargetNonEmpty(object):
     '''This is mixin class that can be added to a luigi task to cause it to fail if the produced output is exists but is empty
         Handles checking compressed files which can have nonzero size but still be empty'''
+
     def complete(self):
         outputs = luigi.task.flatten(self.output())
         return super().complete() and all(map(is_not_empty, [x.path for x in outputs]))
@@ -60,14 +68,15 @@ class CheckTargetNonEmpty(object):
 #                           Parsing STAR logs                                  #
 ###############################################################################
 keep = [
-        'Number of input reads',
-        'Average input read length',
-        'Uniquely mapped reads number',
-        'Uniquely mapped reads %',
-        'Average mapped length',
-        'Mismatch rate per base, %',
-        'Started job on',
-        ]
+    'Number of input reads',
+    'Average input read length',
+    'Uniquely mapped reads number',
+    'Uniquely mapped reads %',
+    'Average mapped length',
+    'Mismatch rate per base, %',
+    'Started job on',
+]
+
 
 def parseStarLog(logfile, lib):
     '''Parse the star Log.final.out file :param: logfile for the library :param: lib 
@@ -78,32 +87,37 @@ def parseStarLog(logfile, lib):
             for line in f:
                 split = line.strip().split(" |\t")
                 if len(split) == 2 and split[0] in keep:
-                    s[split[0]] = float(split[1][:-1]) if '%' in split[1] else split[1] 
+                    s[split[0]] = float(split[1][:-1]) if '%' in split[1] else split[1]
     except FileNotFoundError:
-        pd.Series(dict(zip(keep,[float('nan')]*len(keep))))
-    
-    s['Library'] = lib    
+        pd.Series(dict(zip(keep, [float('nan')] * len(keep))))
+
+    s['Library'] = lib
     return s
+
 
 def parseLibList(liblist, base_dir):
     return pd.DataFrame([parseStarLog(os.path.join(base_dir, lib, 'Log.final.out'), lib) for lib in liblist]).set_index('Library')
-    
-    
+
+
 ###############################################################################
 #                                    Git                                     #
 ###############################################################################
-    
+
 def file_hash(file):
-    r = subprocess.run("git hash-object " + file, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+    r = subprocess.run("git hash-object " + file, shell=True, check=True,
+                       stdout=subprocess.PIPE, universal_newlines=True)
     return r.stdout.strip()
 
+
 def current_commit_hash(git_dir):
-    r = subprocess.run("git rev-parse HEAD ", shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True, cwd=git_dir)
+    r = subprocess.run("git rev-parse HEAD ", shell=True, check=True,
+                       stdout=subprocess.PIPE, universal_newlines=True, cwd=git_dir)
     return r.stdout.strip()
 
 ###############################################################################
 #                          Pipeline hashing                                   #
 ###############################################################################
+
 
 def ancestor(task):
     '''Return a set of all Tasks that are ancestors of :param: task'''
@@ -114,6 +128,7 @@ def ancestor(task):
             tree.update(ancestor(d))
     return list(tree)
 
+
 def hash_pipeline(task):
     '''Creates a hash of the source code of :param: task and all tasks upstream of it'''
     sha = hashlib.sha1()
@@ -121,11 +136,12 @@ def hash_pipeline(task):
     sources = sorted([inspect.getsource(type(t)) for t in task_list])
     for s in sources:
         sha.update(s.encode())
-        
+
     return sha.hexdigest()
-    
+
+
 def hash_files(task):
     '''Get all of the files used in any task upstream of :parm: task and returns there git file hash'''
     task_list = [task] + list(ancestor(task))
     files = [inspect.getfile(type(t)) for t in task_list]
-    return {f:git.git_hash(f) for f in files}
+    return {f: git.git_hash(f) for f in files}

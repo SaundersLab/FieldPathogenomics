13093c0c880e4b84eefbc8d57ccee1892135e586
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2016-12-18 15:16:17 +0000

    autoPEP8

--- fieldpathogenomics/luigi/scattergather.py
+++ fieldpathogenomics/luigi/scattergather.py
@@ -3,8 +3,11 @@ import luigi
 from luigi import Target, LocalTarget
 from luigi.util import task_wraps, inherits, requires
 from luigi.task import getpaths
-import six,os,math
+import six
+import os
+import math
 import copy
+
 
 def indextarget(struct, idx):
     """
@@ -14,12 +17,13 @@ def indextarget(struct, idx):
         base, *ext = struct.path.split('.', maxsplit=1)
         if len(ext) > 0:
             return LocalTarget(base + "_" + str(idx) + "." + ext[0])
-        else :
+        else:
             return LocalTarget(base + "_" + str(idx))
 #    elif isinstance(struct, list):
 #        indextarget(struct[0], idx)
     else:
         raise NotImplemented()
+
 
 class ScatterGather():
     '''Decorator to transparently add Scatter-Gather parallelism to a Luigi task
@@ -28,10 +32,10 @@ class ScatterGather():
     :param scatterTask must inherit and implement a run() method which maps
            an array of input() files to a single output() file
     :param N the number of parts to scatter into
-    
+
     Example
     =======
-    
+
     class scatter(luigi.Task):
         def run(self):
             with self.input().open() as fin:
@@ -40,7 +44,7 @@ class ScatterGather():
             for i,out in enumerate(self.output()):
                 with out.open('w') as fout:
                     fout.writelines(inp[i*perfile:(i+1)*perfile])
-                
+
 
     class gather(luigi.Task):
         def run(self):
@@ -48,8 +52,8 @@ class ScatterGather():
                 for i in self.input():
                     with i.open('r') as fin:
                         fout.write(fin.read())
-                    
-                    
+
+
     @ScatterGather(scatter, gather, 10) 
     class ToBeScattered(luigi.Task):
         def run(self):
@@ -57,74 +61,83 @@ class ScatterGather():
                 with self.output().open('w') as fout:
                     for l in fin:
                         fout.write("Done! " + l)
-    
+
     '''
+
     def __init__(self, scatterTask, gatherTask, N):
         self.scatterTask = scatterTask
         self.gatherTask = gatherTask
         self.N = N
-        
 
     def metaProgScatter(self, scattertask):
         meta_self = self
-                
+
         @inherits(self.workTask)
         class Scatter(scattertask):
 
             def requires(self):
                 wt_req = meta_self.workTask.requires(self)
                 return wt_req[0] if isinstance(wt_req, list) else wt_req
+
             def output(self):
                 return [indextarget(meta_self.workTask.input(self), i) for i in range(meta_self.N)]
-            def to_str_params(self,only_significant=False):
+
+            def to_str_params(self, only_significant=False):
                 sup = super().to_str_params(only_significant)
-                extras = {'input':self.input().path, 'N':str(meta_self.N)}
+                extras = {'input': self.input().path, 'N': str(meta_self.N)}
                 return dict(list(sup.items()) + list(extras.items()))
-        
+
         return Scatter
-        
+
     def metaProgWork(self, worktask):
         meta_self = self
+
         class Work(worktask):
             SG_index = luigi.IntParameter()
+
             @property
             def task_family(self):
                 return worktask.task_family
+
             def requires(self):
                 s = self.clone(meta_self.Scatter)
-                return [s]+super().requires()[1:] if isinstance(super().requires(), list) else [s]
+                return [s] + super().requires()[1:] if isinstance(super().requires(), list) else [s]
+
             def input(self):
                 inp = super().input()
                 scattered = inp[0][self.SG_index]
-                return [inp[0][self.SG_index]] + inp[1:]  if isinstance(super().requires(), list) else inp[0][self.SG_index]
+                return [inp[0][self.SG_index]] + inp[1:] if isinstance(super().requires(), list) else inp[0][self.SG_index]
+
             def output(self):
                 return indextarget(super().output(), self.SG_index)
         return Work
 
     def metaProgGather(self, gathertask):
         meta_self = self
-                
+
         @inherits(self.workTask)
         class Gather(gathertask):
             SG_index = None
+
             def requires(self):
-                return  [self.clone(meta_self.Work, SG_index=i) for i  in range(meta_self.N)]
+                return [self.clone(meta_self.Work, SG_index=i) for i in range(meta_self.N)]
+
             def output(self):
                 return meta_self.workTask.output(self)
-            def to_str_params(self,only_significant=False):
+
+            def to_str_params(self, only_significant=False):
                 sup = super().to_str_params(only_significant)
-                extras = {'N':str(meta_self.N), 'output':self.output().path}
+                extras = {'N': str(meta_self.N), 'output': self.output().path}
                 return dict(list(sup.items()) + list(extras.items()))
-                
+
         return Gather
-        
+
     def __call__(self, workTask):
-        
+
         self.workTask = workTask
-        
+
         self.Scatter = self.metaProgScatter(self.scatterTask)
         self.Work = self.metaProgWork(self.workTask)
         self.Gather = self.metaProgGather(self.gatherTask)
-        
+
         return self.Gather
-        
--- fieldpathogenomics/luigi/slurm.py
+++ fieldpathogenomics/luigi/slurm.py
@@ -1,4 +1,6 @@
-import os,sys, re
+import os
+import sys
+import re
 import subprocess
 import time
 import logging
@@ -10,18 +12,19 @@ import luigi
 alloc_log = logging.getLogger('alloc_log')
 logger = logging.getLogger('luigi-interface')
 
+
 class SlurmMixin(object):
     '''Mixin to support running Task on a SLURM cluster
-   
+
      Parameters:
 
     - n_cpu: Number of CPUs to allocate for the Task. 
     - mem: Amount of memory to require MB
     - partition: slurm partition to submit to
     - run_locally: Run locally instead of on the cluster.
-    
+
      '''
-    
+
     def _init_tmp(self):
         # Set up temp folder in shared directory
         base_tmp_dir = tempfile.gettempdir()
@@ -40,52 +43,57 @@ class SlurmMixin(object):
     def _salloc(self):
         '''Request a job allocation from the scheduler, blocks until its ready then return the job id '''
         salloc = "salloc -N 1 -c {n_cpu} -n 1 --mem {total_mem} -p {partition} -J {job_name} --no-shell".format(
-        n_cpu=self.n_cpu, partition=self.partition, total_mem=int(self.mem*self.n_cpu), job_name=self.job_name)
-        
-        comp = subprocess.run(salloc, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True, check=True)
+            n_cpu=self.n_cpu, partition=self.partition, total_mem=int(self.mem * self.n_cpu), job_name=self.job_name)
+
+        comp = subprocess.run(salloc, shell=True, stderr=subprocess.PIPE,
+                              stdout=subprocess.PIPE, universal_newlines=True, check=True)
         grant_id = re.compile('salloc: Granted job allocation (\S+)')
-        
+
         for line in comp.stderr.split('\n'):
             if grant_id.match(line) is not None:
                 return (grant_id.match(line).groups()[0])
-                
+
         raise Exception("Unable to create job allocation: " + comp.stderr)
 
     def _srun(self, launch, alloc):
         '''Run the task in launch in allocation alloc'''
         srun = "srun -n 1 --kill-on-bad-exit  --quit-on-interrupt --jobid {jobid} -c {n_cpu} --mem-per-cpu {mem}  -o {outfile} -e {errfile} {launch}".format(
-        n_cpu=self.n_cpu, jobid=alloc, mem=self.mem, launch=launch, outfile=self.outfile, errfile=self.errfile )
+            n_cpu=self.n_cpu, jobid=alloc, mem=self.mem, launch=launch, outfile=self.outfile, errfile=self.errfile)
         ret = subprocess.run(srun, shell=True, check=True)
 
     def _slaunch(self, launch):
         return "salloc --quiet -N 1 -c {n_cpu} -n 1 --mem {total_mem} -p {partition} -J {job_name}  srun  -n 1 -c {n_cpu} --mem-per-cpu {mem} {launch} > {outfile} 2> {errfile}".format(n_cpu=self.n_cpu,
-         mem=self.mem, partition=self.partition, job_name=self.job_name, launch=launch, outfile=self.outfile, errfile=self.errfile )
+                                                                                                                                                                                        mem=self.mem, partition=self.partition, job_name=self.job_name, launch=launch, outfile=self.outfile, errfile=self.errfile)
 
     def _fetch_task_failures(self):
         '''Handles reading either the local CompletedProcess or the SLURM log files'''
         ret = ''
         if self.run_locally:
             if not self.completedprocess.stdout is None:
-                ret+= self.completedprocess.stdout.replace("\n", "\nstdout " + self.task_id + ": ") 
+                ret += self.completedprocess.stdout.replace("\n", "\nstdout " + self.task_id + ": ")
             if not self.completedprocess.stderr is None:
-                ret+= self.completedprocess.stderr.replace("\n", "\nstdout " + self.task_id + ": ")
+                ret += self.completedprocess.stderr.replace("\n", "\nstdout " + self.task_id + ": ")
         else:
             try:
                 with open(self.errfile, 'r') as err:
-                    ret +="\nSLURM err " + self.task_id + ": " + err.read().replace("\n", "\nSLURM err " + self.task_id + ": ") 
-            except (FileNotFoundError,AttributeError):
-                ret +="\nSLURM err " + self.task_id + ": " + "None"
-            try: 
+                    ret += "\nSLURM err " + self.task_id + ": " + \
+                        err.read().replace("\n", "\nSLURM err " + self.task_id + ": ")
+            except (FileNotFoundError, AttributeError):
+                ret += "\nSLURM err " + self.task_id + ": " + "None"
+            try:
                 with open(self.outfile, 'r') as out:
-                    ret +="\nSLURM out " + self.task_id + ": " + out.read().replace("\n", "\nSLURM out " + self.task_id + ": ") 
-            except (FileNotFoundError,AttributeError):
-                ret +="\nSLURM out " + self.task_id + ": " + "None"
-            
+                    ret += "\nSLURM out " + self.task_id + ": " + \
+                        out.read().replace("\n", "\nSLURM out " + self.task_id + ": ")
+            except (FileNotFoundError, AttributeError):
+                ret += "\nSLURM out " + self.task_id + ": " + "None"
+
         return ret
-        
+
     def scancel(self):
         if self.alloc is not None:
             subprocess.run("scancel {0}".format(self.alloc), shell=True, check=False)
+
+
 class SlurmExecutableTask(luigi.Task, SlurmMixin):
 
     """
@@ -96,39 +104,42 @@ class SlurmExecutableTask(luigi.Task, SlurmMixin):
     n_cpu = luigi.IntParameter(default=1, significant=False)
     mem = luigi.IntParameter(default=1000, significant=False)
     partition = luigi.Parameter(default='tgac-medium', significant=False)
-    run_locally = luigi.BoolParameter(significant=False, description="run locally instead of on the cluster")
+    run_locally = luigi.BoolParameter(
+        significant=False, description="run locally instead of on the cluster")
     rm_tmp = luigi.BoolParameter(default=True, significant=False)
-    
+
     def __init__(self, *args, **kwargs):
         super(SlurmExecutableTask, self).__init__(*args, **kwargs)
         self.job_name = self.task_family
 
     def run(self):
-        self._init_tmp()            
+        self._init_tmp()
         self.launcher = os.path.join(self.tmp_dir, "launch.sh")
-        
+
         with open(self.launcher, 'w') as l:
             l.write(self.work_script())
-            
+
         if self.run_locally:
-            self.completedprocess = subprocess.run(self.launcher , shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+            self.completedprocess = subprocess.run(
+                self.launcher, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
             self.completedprocess.check_returncode()
-            
+
         else:
             self.outfile = os.path.join(self.tmp_dir, 'job.out')
             self.errfile = os.path.join(self.tmp_dir, 'job.err')
-            
+
             self.alloc = None
             try:
                 self.alloc = self._salloc()
-                logger.info("SLURM: jobid={0}".format(self.alloc) )
+                logger.info("SLURM: jobid={0}".format(self.alloc))
                 alloc_log.info(self.task_id + "\t" + str(self.alloc))
-                
+
                 self._srun(self.launcher, self.alloc)
-                
+
             finally:
                 # Always be sure to free the slurm allocation
                 self.scancel()
+
     def on_failure(self, exception):
         err = self._fetch_task_failures()
         self.clear_tmp()
@@ -148,7 +159,7 @@ class SlurmExecutableTask(luigi.Task, SlurmMixin):
             return err + "\n" + super_retval
         else:
             return err
-    
+
     def work_script(self):
         """Override this an make it return the shell script to run"""
         pass

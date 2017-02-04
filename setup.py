bfee0df0610795fc173d2e00978bf8a89097baaa
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2017-02-04 14:46:33 +0000

    Add version file

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+exec(open('fieldpathogenomics/version.py').read())
+
 setup(
     name = "FieldPathogenomics",
-    version = "0.0.0",
+    version = __version__,
     author = "Daniel Bunintg",
     author_email = "daniel.bunting@earlham.ac.uk",
     url = "https://github.com/dnlbunting/FieldPathogenomics/",

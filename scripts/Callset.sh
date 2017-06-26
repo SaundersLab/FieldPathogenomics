4e7b3d42cc0411348bcd6f7ccabdaf881d275662
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2017-06-26 16:38:51 +0100

    Fix base_dir path

--- scripts/Callset.sh
+++ scripts/Callset.sh
@@ -1,6 +1,6 @@
-sbatch --mem 24000 -N 1 -n 1 -c 1 -p nbi-long -J Callset --wrap="cd $base_dir; source $dev_dir/dev/bin/activate; \
+sbatch --mem 8000 -N 1 -n 1 -c 1 -p nbi-long -J Callset --wrap="cd $base_dir; source $dev_dir/dev/bin/activate; \
 srun python -m fieldpathogenomics.pipelines.Callset $1 $2 \
---base-dir $base_dir/data \
+--base-dir $base_dir \
 --scratch-dir $scratch_dir \
 --workers 300"
 

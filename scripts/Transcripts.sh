df76a8354077062ada67c8efe8231338eb424f1b
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2017-06-27 13:28:58 +0100

    Fix wrapper script names

--- scripts/Transcripts.sh
+++ scripts/Transcripts.sh
@@ -1,4 +1,4 @@
-sbatch --mem 8000 -N 1 -n 1 -c 1 -p nbi-long -J Callset --wrap="cd $base_dir; source $dev_dir/dev/bin/activate; \
+sbatch --mem 8000 -N 1 -n 1 -c 1 -p nbi-long -J Transcripts --wrap="cd $base_dir; source $dev_dir/dev/bin/activate; \
 srun python -m fieldpathogenomics.pipelines.Transcripts $1 \
 --base-dir $base_dir \
 --scratch-dir $scratch_dir \
